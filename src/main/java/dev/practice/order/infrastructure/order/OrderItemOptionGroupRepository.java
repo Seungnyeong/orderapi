dfdf66ebac2dd6d81095c54841762dc412e342f9
김승녕 <workingsnkim@gmail.com>
2022-04-07 15:08:23 +0900

    Order Infrasturceture part

---
+++ src/main/java/dev/practice/order/infrastructure/order/OrderItemOptionGroupRepository.java
@@ -0,0 +1,6 @@
+package dev.practice.order.infrastructure.order;
+import dev.practice.order.domain.order.item.OrderItemOptionGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemOptionGroupRepository extends JpaRepository<OrderItemOptionGroup, Long> {
+}
